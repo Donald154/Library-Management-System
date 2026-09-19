4831e61f5d5e01621de5f5c029faddcd9c2dc2cd
Donald Pughsley <donaldpughsleys@gmail.com>
2026-09-19 16:38:36 -0400

    Foundation for Main app

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
+//Main application
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Create LMS object
+        LibraryManagementSystem lms = new LibraryManagementSystem();
+
+        System.out.println("========================= Library Management System =========================");
+
+        //Ask user for patron txt file
+        System.out.println("Enter the file path for the patron text file");
+        String filePath = scanner.nextLine();
+
+        //Import patrons
+        lms.importPatrons(filePath);
+
+        //Display imported patrons
+        lms.displayPatron();
+        boolean running = true;
+
+
+    }
+
+
 }
